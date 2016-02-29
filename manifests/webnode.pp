f5a41b8ef71f26f773df614e124f9dd25d0f18ce
Martyn Ranyard <martyn@ranyard.info>
2016-02-29 23:56:09 +0000

    Inital cut at loadbalancing

--- manifests/webnode.pp
+++ manifests/webnode.pp
@@ -1,16 +1,24 @@
-class apache ($version = "latest") {
-          package {"apache2":
-            ensure => $version, # Using the class parameter from above
-          }
-          file {"/var/www/index.html":
-            ensure  => present,
-            content => "<html>\n<body>\n\t<h2>Hi, this is $gce_external_ip.</h2>\n</body>\n</html>\n",
-            require => Package["apache2"],
-          }
-          service {"apache2":
-            ensure => running,
-            enable => true,
-            require => File["/var/www/index.html"],
-          }
-        }
-include apache
+class nginx ($version = "latest") {
+    package {"nginx":
+        ensure => $version, # Using the class parameter from above
+    }
+    file {"loadbalance-available":
+        path => "/etc/nginx/sites-available/loadbalance",
+        content => template("gce_example/loadbalance.erb"),
+        notify => Service['nginx'],
+        require => Package['nginx'],
+    }
+    file {'loadbalance-enabled':
+        path => '/etc/nginx/sites-enabled/loadbalance',
+        ensure => 'link',
+        target => '/etc/nginx/sites-available/loadbalance',
+        notify => Service['nginx'],
+        require => File['loadbalance-available']
+    }
+    service {"nginx":
+        ensure => running,
+        enable => true,
+        require => File["/etc/nginx/sites-enabled/loadbalance"],
+    }
+}
+include nginx
