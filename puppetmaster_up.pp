c52a13baac3384fb20fb05360ab1187b9a79f78a
Martyn Ranyard <martyn@ranyard.info>
2016-03-01 00:31:03 +0000

    Typod my own name

--- puppetmaster_up.pp
+++ puppetmaster_up.pp
@@ -1,4 +1,4 @@
-gce_instance { 'puppet-master':
+gce_instance { 'puppetmaster':
   ensure                => present,
   description           => 'Puppet Master Open Source',
   machine_type          => 'f1-micro',
@@ -9,11 +9,11 @@ gce_instance { 'puppet-master':
   puppet_manifest       => "../../gce_compute_master/manifests/init.pp",
   startup_script        => 'puppet-community.sh',
   puppet_service        => present,
-  puppet_master         => "puppet-master",
+  puppet_master         => "puppetmaster",
   puppet_modules        => ['puppetlabs-gce_compute', 'puppetlabs-inifile', 'puppetlabs-stdlib', 'puppetlabs-apt', 'puppetlabs-concat', 'saz-locales'],
   puppet_module_repos   => { 
     'puppetlabs-gce_compute' => "git://github.com/puppetlabs/puppetlabs-gce_compute",
     'gce_compute_master' => 'git://github.com/iMartyn/puppet-gce_compute_master',
-    'gce_example' => 'git://github.com/iMartun/puppet-gce_example'
+    'gce_example' => 'git://github.com/iMartyn/puppet-gce_example'
   }
 }
